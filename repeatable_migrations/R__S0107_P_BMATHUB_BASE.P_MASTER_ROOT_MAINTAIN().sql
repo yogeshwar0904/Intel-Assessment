3e957366396cb15d3131063a9ec88ffdc87a7664
yogeshwar0904 <genithayogeshwar@gmail.com>
2024-06-12 15:32:07 +0530

    created rootcorebuild procedure

--- repeatable_migrations/R__S0107_P_BMATHUB_BASE.P_MASTER_ROOT_MAINTAIN().sql
+++ repeatable_migrations/R__S0107_P_BMATHUB_BASE.P_MASTER_ROOT_MAINTAIN().sql
@@ -24,19 +24,19 @@ BEGIN
             SRC.ITEM_CLASS_NM,
             SRC.LOCATION_ID
         );
-
     MERGE INTO 
         Build_SIM_BOM."BMATHUB_BASE".T_ITEM_DETAIL_ROOT AS TGT
     USING  
         Build_SIM_BOM."MASTER_DATA".ITEM AS SRC
     ON 
         TGT.ITEM_ID = SRC.ITEM_ID
+        AND SRC.DELETE_IND = 'FALSE'    
     WHEN MATCHED THEN 
         UPDATE SET
             TGT.ITEM_CLASS_NM = SRC.ITEM_CLASS_NM,
             TGT.ITEM_DSC = SRC.ITEM_DSC,
             TGT.DELETE_IND = SRC.DELETE_IND
-    WHEN NOT MATCHED THEN 
+    WHEN NOT MATCHED  AND SRC.DELETE_IND = 'FALSE' THEN 
         INSERT (
             ITEM_ID,
             ITEM_CLASS_NM,
@@ -48,14 +48,13 @@ BEGIN
             SRC.ITEM_DSC,
             SRC.DELETE_IND 
         );
-
     MERGE INTO 
         Build_SIM_BOM."BMATHUB_BASE".T_ORIG_BOM AS TGT
     USING  
         Build_SIM_BOM."MASTER_DATA".ORIG_BOM AS SRC
     ON 
         TGT.INPUT_ITEM_ID = SRC.INPUT_ITEM_ID 
-    AND TGT.OUTPUT_ITEM_ID = SRC.OUTPUT_ITEM_ID      
+    AND TGT.OUTPUT_ITEM_ID = SRC.OUTPUT_ITEM_ID  
     WHEN MATCHED THEN 
         UPDATE SET
             TGT.ITEM_CLASS_NM = SRC.ITEM_CLASS_NM,
@@ -72,7 +71,7 @@ BEGIN
             SRC.OUTPUT_ITEM_ID,
             SRC.LOC 
         );
-    RETURN 'Insert into table successfully';
+    RETURN 'Data Insert into table successfully';
 END;
 $$;
-call Build_SIM_BOM."BMATHUB_BASE".P_MASTER_ROOT_MAINTAIN();
+call Build_SIM_BOM."BMATHUB_BASE".P_MASTER_ROOT_MAINTAIN();  
